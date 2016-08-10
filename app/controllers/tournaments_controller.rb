2792d06a24fd2f49018afea55c6ebf291740ea18
Criticalstone <kevinhoog@gmail.com>
2016-08-10 19:04:51 +0200

    Adds new and edit function to tournament

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -2,4 +2,28 @@ class TournamentsController < ApplicationController
 	def index
 		@tournaments = Tournament.all
 	end
+
+	def new
+		@tournament = Tournament.new
+	end
+
+	def create 
+		@tournament = Tournament.new(tournament_params)
+
+		if @tournament.save
+			redirect_to @tournament
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@tournament = Tournament.find(params[:id])
+	end
+
+	private
+
+	def tournament_params
+		params.require(:tournament).permit(:name, :description, :game, :team_size, :developer, :image, :rules)
+	end
 end
