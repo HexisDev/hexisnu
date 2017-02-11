d1069cfdb6963245ad786348ed4693261eb4850d
Criticalstone <kevinhoog@gmail.com>
2017-02-11 15:56:03 +0100

    Adds group tables and tickets left counter

--- app/helpers/tables_helper.rb
+++ app/helpers/tables_helper.rb
@@ -2,4 +2,14 @@ module TablesHelper
     def height(table)
         return (100*(table.booked.to_f/table.seats)).round(0)
     end
+
+    def tickets_left()
+        seats = 0
+        booked = 0
+        Table.all.each do |t|
+            booked += t.booked
+            seats += t.seats
+        end 
+        return seats - booked
+    end
 end
